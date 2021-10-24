4752680c775c07501c7f2cb06e6f60561a34d837
mutotu <muttalip.aydemir@gmail.com>
2021-10-23 19:06:35 -0700

    Muttalip, working timer

--- script.js
+++ script.js
@@ -1,8 +1,101 @@
+// "use strcit";
+
+// const IMAGES = [
+//   "image1.jpg",
+//   "image2.jpg",
+//   "image3.jpg",
+//   "image4.jpg",
+//   "image5.jpg",
+//   "image6.jpg",
+//   "image7.jpg",
+//   "image8.jpg",
+//   "image9.jpg",
+//   "image10.jpg",
+//   "image11.jpg",
+//   "image12.jpg",
+//   "image13.jpg",
+//   "image14.jpg",
+//   "image15.jpg",
+//   "image16.jpg",
+//   "image17.jpg",
+//   "image18.jpg",
+//   "image19.jpg",
+//   "image20.jpg",
+//   "image21.jpg",
+//   "image22.jpg",
+//   "image23.jpg",
+//   "image24.jpg",
+// ];
+
+// let section = document.querySelector("section");
+// section.addEventListener("click", () => console.log(section));
+// const sectionAll = document.querySelector("section");
+// const sectionEls = Array.from(document.querySelectorAll("section > div"));
+// let copyImages = [...IMAGES];
+// let placeImages;
+// let indexCollector = [];
+
+// function init() {
+//   placeImages = randomPhotoGenerator(copyImages);
+//   function randomPhotoGenerator(array) {
+//     let counter = array.length / 2;
+//     let random = 0;
+//     let result = [];
+//     while (counter--) {
+//       random = Math.floor(Math.random() * (counter + 1));
+//       result.push(array[random]);
+//       array.splice(random, 1);
+//     }
+//     result = [...result, ...result];
+//     //Shuffle the array
+//     return result.sort(() => Math.random() - 0.5);
+//   }
+
+//   function imgPlacer() {
+//     for (let i = 0; i < placeImages.length; i++) {
+//       sectionEls[i].setAttribute("name", placeImages[i]);
+//       sectionEls[i].setAttribute("id", i);
+
+//       sectionEls[i].style.backgroundColor = "blue";
+
+//       sectionEls[i].addEventListener("click", function () {
+//         let img = document.createElement("img");
+//         img.src = "images/" + sectionEls[i].getAttribute("name");
+//         img.style.height = "16vmin";
+//         sectionEls[i].appendChild(img);
+//         indexCollector.push(i);
+
+//         checkMatch();
+//       });
+//     }
+//   }
+//   function checkMatch() {
+//     if (indexCollector.length === 2) {
+//       let item1 = document.getElementById(indexCollector[0]);
+//       let item2 = document.getElementById(indexCollector[1]);
+
+//       if (item1.getAttribute("name") === item2.getAttribute("name")) {
+//         item1.setAttribute("disabled", true);
+//         item2.setAttribute("disabled", true);
+//         indexCollector = [];
+//       } else {
+//         setTimeout(() => {
+//           item1.innerHTML = "";
+//           item2.innerHTML = "";
+//         }, 1000);
+//         indexCollector = [];
+//       }
+//     }
+//   }
+
+//   imgPlacer();
+// }
+
+// window.onload = init();
+
+/////////////////
 "use strcit";
 
-//Model
-
-//All uppercase  for lookups -const
 const IMAGES = [
   "image1.jpg",
   "image2.jpg",
@@ -30,16 +123,18 @@ const IMAGES = [
   "image24.jpg",
 ];
 
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
 let section = document.querySelector("section");
 section.addEventListener("click", () => console.log(section));
+const playBt = document.querySelector("button");
 const sectionAll = document.querySelector("section");
-const sectionEls = Array.from(document.querySelectorAll("section > div"));
+let sectionEls = Array.from(document.querySelectorAll("section > div"));
 let copyImages = [...IMAGES];
 let placeImages;
 let indexCollector = [];
-let twoItems = [];
-
-////
+let counter = 0;
+let countTime = 0;
 function init() {
   placeImages = randomPhotoGenerator(copyImages);
   function randomPhotoGenerator(array) {
@@ -55,22 +150,22 @@ function init() {
     //Shuffle the array
     return result.sort(() => Math.random() - 0.5);
   }
-  ///first append the name
-  ///then if names match append the photo
+
   function imgPlacer() {
     for (let i = 0; i < placeImages.length; i++) {
       sectionEls[i].setAttribute("name", placeImages[i]);
       sectionEls[i].setAttribute("id", i);
 
       sectionEls[i].style.backgroundColor = "blue";
-      //
+
       sectionEls[i].addEventListener("click", function () {
         let img = document.createElement("img");
         img.src = "images/" + sectionEls[i].getAttribute("name");
-        img.style.height = "16vmin";
+        img.style.height = "12vmin";
+
         sectionEls[i].appendChild(img);
         indexCollector.push(i);
-        // sectionEls[i].append(img);
+
         checkMatch();
       });
     }
@@ -81,6 +176,7 @@ function init() {
       let item2 = document.getElementById(indexCollector[1]);
 
       if (item1.getAttribute("name") === item2.getAttribute("name")) {
+        counter++;
         item1.setAttribute("disabled", true);
         item2.setAttribute("disabled", true);
         indexCollector = [];
@@ -88,14 +184,28 @@ function init() {
         setTimeout(() => {
           item1.innerHTML = "";
           item2.innerHTML = "";
-        }, 1000);
+        }, 500);
         indexCollector = [];
-        // item1.innerHTML = "";
-        // item2.innerHTML = "";
       }
     }
   }
 
+  function score() {
+    if (counter === placeImages.length / 2) {
+      h2.innerText = "Your score is somethin";
+    }
+  }
+
+  function timer() {
+    let time = setInterval(function () {
+      h3.innerText = "Time: " + ++countTime;
+      if (counter === 12) {
+        clearInterval(time);
+      }
+    }, 1000);
+  }
+  timer();
+  score();
   imgPlacer();
 }
 
